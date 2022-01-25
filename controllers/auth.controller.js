686ae27a2fe92829babcc90f3d99f96532167393
duncan4123 <54425677+duncan4123@users.noreply.github.com>
2022-01-25 21:50:21 +1100

    update redirect

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,52 +1,55 @@
 import {
   getOAuthAccessTokenWith,
   getOAuthRequestToken,
-  oauthGetUserById,
-} from "../oauth-utils.js";
+  oauthGetUserById
+} from '../oauth-utils.js'
 
-export const twitter =
-  (method = "authorize") =>
-  async (req, res) => {
-    try {
-      const { oauthRequestToken, oauthRequestTokenSecret } =
-        await getOAuthRequestToken();
-
-      req.session = req.session || {};
-      req.session.oauthRequestToken = oauthRequestToken;
-      req.session.oauthRequestTokenSecret = oauthRequestTokenSecret;
-
-      const authorizationUrl = `https://api.twitter.com/oauth/${method}?oauth_token=${oauthRequestToken}`;
-      res.redirect(authorizationUrl);
-    } catch (error) {
-      res.status(400).json({ message: `Could not ${method} user`, error });
-    }
-  };
-export const handleTwitterCallback = async (req, res) => {
-  const { oauthRequestToken, oauthRequestTokenSecret } = req.session;
-  const { oauth_verifier: oauthVerifier } = req.query;
-
-  const { oauthAccessToken, oauthAccessTokenSecret, results } =
-    await getOAuthAccessTokenWith({
+export const twitter = (method = 'authorize') => async (req, res) => {
+  try {
+    const {
       oauthRequestToken,
-      oauthRequestTokenSecret,
-      oauthVerifier,
-    });
-  req.session.oauthAccessToken = oauthAccessToken;
+      oauthRequestTokenSecret
+    } = await getOAuthRequestToken()
 
-  const { user_id: userId /*, screen_name */ } = results;
-  const user = await oauthGetUserById(userId, {
+    req.session = req.session || {}
+    req.session.oauthRequestToken = oauthRequestToken
+    req.session.oauthRequestTokenSecret = oauthRequestTokenSecret
+
+    const authorizationUrl = `https://api.twitter.com/oauth/${method}?oauth_token=${oauthRequestToken}`
+    res.redirect(authorizationUrl)
+  } catch (error) {
+    res.status(400).json({ message: `Could not ${method} user`, error })
+  }
+}
+export const handleTwitterCallback = async (req, res) => {
+  const { oauthRequestToken, oauthRequestTokenSecret } = req.session
+  const { oauth_verifier: oauthVerifier } = req.query
+
+  const {
     oauthAccessToken,
     oauthAccessTokenSecret,
-  });
+    results
+  } = await getOAuthAccessTokenWith({
+    oauthRequestToken,
+    oauthRequestTokenSecret,
+    oauthVerifier
+  })
+  req.session.oauthAccessToken = oauthAccessToken
 
-  req.session.twitter_screen_name = user.screen_name;
-  res.cookie("twitter_screen_name", user.screen_name, {
+  const { user_id: userId /*, screen_name */ } = results
+  const user = await oauthGetUserById(userId, {
+    oauthAccessToken,
+    oauthAccessTokenSecret
+  })
+
+  req.session.twitter_screen_name = user.screen_name
+  res.cookie('twitter_screen_name', user.screen_name, {
     maxAge: 900000,
-    httpOnly: true,
-  });
+    httpOnly: true
+  })
   req.session.save(() =>
     res.redirect(
-      `https://sybil.cre8r.vip/#/delegates/CRE8R?username=${user.screen_name}`
+      `https://amplifi.cre8r.vip/#/delegates/CRE8R?username=${user.screen_name}`
     )
-  );
-};
+  )
+}
