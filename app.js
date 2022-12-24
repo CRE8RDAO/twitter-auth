42dde390746c563c7051393fac7b5ec179ae4f57
dunks411 <54425677+duncan4123@users.noreply.github.com>
2022-12-24 16:37:49 +1100

    more logs

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-import express from "express";
-import AuthRouter from "./routes/auth.routes.js";
-import cookieParser from "cookie-parser";
-import session from "express-session";
-const PORT = process.env.PORT || 5000;
-const app = express();
+import express from 'express'
+import AuthRouter from './routes/auth.routes.js'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
+const PORT = process.env.PORT || 5000
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.use(
   session({
-    secret: "keyboard cat",
+    secret: 'keyboard cat',
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: false
   })
-);
-app.use("/api/auth", AuthRouter);
+)
+app.use('/api/auth', AuthRouter)
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
-}); //test heroku deploy1
+  console.log(`Server is running on PORT ${PORT}`)
+  log('dunks: ', 'more logs')
+}) //test heroku deploy1
